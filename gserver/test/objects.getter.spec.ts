20367b0b5d91f5a2276473b7e1dc8e92d1766d48
alexkrechik <scraych@gmail.com>
2017-02-22 18:19:25 +0200

    Fixed quotemarks

--- gserver/test/objects.getter.spec.ts
+++ gserver/test/objects.getter.spec.ts
@@ -1,9 +1,9 @@
 import { expect } from 'chai';
 import * as objectsGetter from '../src/objects.getter';
 
-describe("Gserver unit tests", function () {
+describe('Gserver unit tests', function () {
 
-    it("validate regular expression for getting steps", function () {
+    it('validate regular expression for getting steps', function () {
         expect(objectsGetter.getStepRegExp().toString()).to.equal(
             (/^(.*)(Given|When|Then)[^'|^"|^\/]*('|"|\/)([^\3]+)\3/i).toString());
     });
