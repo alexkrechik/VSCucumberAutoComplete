8c29b0a779fc22d67ee780c6cef9c3d77fb3f9f5
alexkrechik <scraych@gmail.com>
2017-03-28 17:14:37 +0300

    Added commented commented lines for steps populating testing

--- gserver/test/data/test.steps.js
+++ gserver/test/data/test.steps.js
@@ -5,3 +5,12 @@ this.When(/^I do something$/, function (next) {
 this.When(/^I do another thing$/, function (next) {
     next;
 });
+
+
+//this.When(/^I check one line commets doesn't affect steps$/, function (next) {next;});
+
+/*
+this.When(/^I check multi line commets doesn't affect steps$$/, function (next) {
+    next;
+});
+*/
