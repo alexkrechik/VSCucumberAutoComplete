3ed803a374cb3bde9ded0fc0609b8b54460ef39e
alexkrechik <scraych@gmail.com>
2017-06-03 16:02:55 +0300

    #89, #90, #93

--- gserver/src/format.ts
+++ gserver/src/format.ts
@@ -8,12 +8,18 @@ interface FormatConf {
 const FORMAT_CONF: FormatConf[] = [
     { text: 'Feature:', indents: 0 },
     { text: 'Scenario:', indents: 1 },
+    { text: 'Background:', indents: 1 },
+    { text: 'Scenario Outline:', indents: 1 },
+    { text: 'Examples:', indents: 2 },
     { text: 'Given', indents: 2 },
     { text: 'When', indents: 2 },
     { text: 'Then', indents: 2 },
     { text: 'And', indents: 2 },
     { text: 'But', indents: 2 },
-    { text: '#', indents: 2 }
+    { text: '#', indents: 2 },
+    { text: '@', indents: 2 },
+    { text: '|', indents: 3 },
+    { text: '"""', indents: 3 },
 ];
 
 export function format(indent: string, range: Range, text: string): string {
@@ -23,7 +29,7 @@ export function format(indent: string, range: Range, text: string): string {
 
     //Get formatted array
     let newTextArr = textArr.map(line => {
-        if (line.search(/^\s*$/) !== -1) {
+        if (~line.search(/^\s*$/)) {
             return '';
         } else {
             let foundFormat = FORMAT_CONF.find(conf => line.search(new RegExp('^\\s*' + conf.text)) !== -1);
